6e32a0bd262febf7b1de02a263714328b1b8d378
Sahil Garg <sahilgarg8901@gmail.com>
2018-02-11 19:27:07 +0530

    Pie chart for student report card done

--- PieApp/app/src/main/java/com/example/sahil/pie_app/MainActivity.kt
+++ PieApp/app/src/main/java/com/example/sahil/pie_app/MainActivity.kt
@@ -1,25 +1,52 @@
 package com.example.sahil.pie_app
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.github.mikephil.charting.components.Description
+import com.github.mikephil.charting.components.Legend
 import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.renderer.scatter.CircleShapeRenderer
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
-    var List : MutableList<PieEntry> = mutableListOf(PieEntry(100f,"Math"))
-
+    var List : MutableList<PieEntry> = mutableListOf()
+    var Data : ArrayList<XYData> = arrayListOf(XYData(40f,"Math"),
+            XYData(20f,"Science"), XYData(30f,"English"),
+            XYData(15f,"Hindi"),XYData(25f,"Computer"))
+    var colors : ArrayList<Int> = arrayListOf(Color.BLUE,Color.GREEN,Color.CYAN,Color.GRAY,Color.RED)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         pieChart.holeRadius = 25f
         pieChart.centerText = "Report Card"
+        pieChart.description.text = "Marks in Subjects by Students"
         pieChart.setCenterTextSize(10f)
+        pieChart.setTransparentCircleAlpha(0)
+        pieChart.isRotationEnabled = true
+        pieChart.setHoleColor(Color.TRANSPARENT)
+        pieChart.setCenterTextColor(Color.RED)
+        pieChart.setDrawEntryLabels(true)
+        pieChart.setEntryLabelTextSize(15f)
+        for (a in Data){
+            List.add(PieEntry(a.yData,a.xData))
+        }
+
         val dataSet = PieDataSet(List,"Marks")
+        dataSet.sliceSpace = 2f
+        dataSet.valueTextSize = 12f
+        dataSet.colors = colors
         val data = PieData(dataSet)
         pieChart.data = data
+
+         pieChart.legend.form = Legend.LegendForm.CIRCLE
         pieChart.invalidate()
+
     }
 }
+
+data class XYData (val yData : Float, val xData : String)
