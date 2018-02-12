004ac19657a5be423cbf5bc899b2bb465f9edadc
Sahil Garg <sahilgarg8901@gmail.com>
2018-02-12 22:03:19 +0530

    Working properly

--- PieApp/app/src/main/java/com/example/sahil/pie_app/Controller/MainActivity.kt
+++ PieApp/app/src/main/java/com/example/sahil/pie_app/Controller/MainActivity.kt
@@ -4,7 +4,11 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.util.Range
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import com.example.sahil.pie_app.R
 import com.example.sahil.pie_app.Utilities.ColorKey
@@ -14,6 +18,8 @@ import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
+import java.text.NumberFormat
+import java.text.ParseException
 
 
 class MainActivity : AppCompatActivity() {
@@ -30,16 +36,15 @@ class MainActivity : AppCompatActivity() {
         pieChart.centerText = "Rating"
         pieChart.description.text = "Product Quality"
         pieChart.description.textAlign = Paint.Align.RIGHT
-        pieChart.setCenterTextSize(10f)
+        pieChart.setCenterTextSize(15f)
         pieChart.setTransparentCircleAlpha(0)
         pieChart.isRotationEnabled = true
         pieChart.setHoleColor(Color.TRANSPARENT)
-        pieChart.setCenterTextColor(Color.YELLOW)
+        pieChart.setCenterTextColor(Color.CYAN)
         pieChart.setDrawEntryLabels(true)
         pieChart.setEntryLabelTextSize(15f)
 
         pieChart.legend.form = Legend.LegendForm.SQUARE
-        pieChart.legend.position = Legend.LegendPosition.BELOW_CHART_LEFT
 
         /*for (a in Data){
             List.add(PieEntry(a.yData,a.xData))
@@ -61,11 +66,26 @@ class MainActivity : AppCompatActivity() {
         pieChart.invalidate()*/
         createPie()
     }
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        //outState?.putParcelable(Extra_text,text as Parcelable?)
+        outState?.putFloatArray(ColorKey,EntryColor)
+    }
     fun goodBtnClicked(view : View){
-        if (FloatValue.toString().isEmpty()){
+        if (FloatValue.text.isEmpty()){
             Toast.makeText(this, "Enter Some Value", Toast.LENGTH_SHORT).show()
+        } else if(getFloatFrom(FloatValue.text) !in Range(1f,99f)){
+            Toast.makeText(this, "Enter between 1 to 99", Toast.LENGTH_SHORT).show()
+            FloatValue.text = null
         } else {
-
+            /*EntryColor.set(0,getFloatFrom(FloatValue.text))
+            EntryColor.set(1,100 - getFloatFrom(FloatValue.text))*/
+            List.clear()
+            EntryColor[0] =  getFloatFrom(FloatValue.text)
+            EntryColor[1] = 100 - EntryColor[0]
+            Log.d("check", EntryColor.toString())
+            FloatValue.text = null
+            createPie()
         }
     }
     private fun createPie(){
@@ -75,10 +95,17 @@ class MainActivity : AppCompatActivity() {
         val dataSet = PieDataSet(List, "Percentage")
         dataSet.sliceSpace = 2f
         dataSet.valueTextSize = 12f
-        dataSet.colors = colors
+        dataSet.colors = this.colors
         val data = PieData(dataSet)
         pieChart.data = data
         pieChart.invalidate()
+    }
+    private fun getFloatFrom(txt : Editable) : Float {
+        try {
+            return NumberFormat.getInstance().parse(txt.toString()).toFloat()
+        } catch (e : ParseException) {
+            return 0f
+        }
     }
 }
 
