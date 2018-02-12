e5ff4f4ab1ca8986d46e5397bb60db78153c120b
Sahil Garg <sahilgarg8901@gmail.com>
2018-02-12 23:37:33 +0530

    Cleaned the code

--- PieApp/app/src/main/java/com/example/sahil/pie_app/Controller/MainActivity.kt
+++ PieApp/app/src/main/java/com/example/sahil/pie_app/Controller/MainActivity.kt
@@ -8,7 +8,6 @@ import android.text.Editable
 import android.util.Log
 import android.util.Range
 import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import com.example.sahil.pie_app.R
 import com.example.sahil.pie_app.Utilities.ColorKey
@@ -17,7 +16,6 @@ import com.github.mikephil.charting.data.PieData
 import com.github.mikephil.charting.data.PieDataSet
 import com.github.mikephil.charting.data.PieEntry
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import java.text.NumberFormat
 import java.text.ParseException
 
@@ -26,8 +24,6 @@ class MainActivity : AppCompatActivity() {
     var GoodOrNot : Boolean = true
     var EntryColor : FloatArray = FloatArray(2)
     var List : MutableList<PieEntry> = mutableListOf()
-    /*var Data : ArrayList<XYData> = arrayListOf(XYData(40f, "Math"),
-            XYData(20f, "Science"))*/
     private var colors : ArrayList<Int> = arrayListOf(Color.GREEN,Color.RED)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,30 +41,16 @@ class MainActivity : AppCompatActivity() {
         pieChart.setEntryLabelTextSize(15f)
 
         pieChart.legend.form = Legend.LegendForm.SQUARE
-
-        /*for (a in Data){
-            List.add(PieEntry(a.yData,a.xData))
-        }*/
         if (savedInstanceState != null) {
             EntryColor = savedInstanceState.getFloatArray(ColorKey)
         } else {
             EntryColor[0] = 50f
             EntryColor[1] = 50f
         }
-       /* List.add(PieEntry(EntryColor[0], "Good"))
-        List.add(PieEntry(EntryColor[1], "Not Good"))
-        val dataSet = PieDataSet(List, "Percentage")
-        dataSet.sliceSpace = 2f
-        dataSet.valueTextSize = 12f
-        dataSet.colors = colors
-        val data = PieData(dataSet)
-        pieChart.data = data
-        pieChart.invalidate()*/
         createPie()
     }
     override fun onSaveInstanceState(outState: Bundle?) {
         super.onSaveInstanceState(outState)
-        //outState?.putParcelable(Extra_text,text as Parcelable?)
         outState?.putFloatArray(ColorKey,EntryColor)
     }
     fun goodBtnClicked(view : View){
@@ -78,8 +60,6 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Enter between 1 to 99", Toast.LENGTH_SHORT).show()
             FloatValue.text = null
         } else {
-            /*EntryColor.set(0,getFloatFrom(FloatValue.text))
-            EntryColor.set(1,100 - getFloatFrom(FloatValue.text))*/
             List.clear()
             EntryColor[0] =  getFloatFrom(FloatValue.text)
             EntryColor[1] = 100 - EntryColor[0]
